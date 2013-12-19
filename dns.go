e09acd84639a58cfd272fad1c65a0deda5b3132b
young wu <public0821@gmail.com>
2013-12-19 21:54:11 +0800

    add main

---
+++ dns.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+    "net"
+)
+
+func main(){
+    addr := net.UDPAddr{Port:5354}
+    conn, err := net.ListenUDP("udp", &addr)
+    if err != nil{
+        fmt.Println(err)
+        return;
+    }
+    buf := make([]byte, 65535)
+
+    for{
+        length, client, err := conn.ReadFromUDP(buf)
+        if err != nil{
+            fmt.Println(err)
+            conn.Close()
+            return;
+        }
+        fmt.Println(client)
+        fmt.Println(string(buf[:length]))
+    }
+    fmt.Println(addr)
+}
