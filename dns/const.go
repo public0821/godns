111542c05eaae71d2408fb44573d34a3e97898a5
young wu <public0821@gmail.com>
2014-01-13 22:33:30 +0800

    add const.go

---
+++ dns/const.go
@@ -0,0 +1,127 @@
+package dns
+
+const (
+	QR_REQUEST  uint16 = 0
+	QR_RESPONSE uint16 = 1
+
+	// valid RRHeader.Type and Question.Type
+	TYPE_None       uint16 = 0
+	TYPE_A          uint16 = 1
+	TYPE_NS         uint16 = 2
+	TYPE_MD         uint16 = 3
+	TYPE_MF         uint16 = 4
+	TYPE_CNAME      uint16 = 5
+	TYPE_SOA        uint16 = 6
+	TYPE_MB         uint16 = 7
+	TYPE_MG         uint16 = 8
+	TYPE_MR         uint16 = 9
+	TYPE_NULL       uint16 = 10
+	TYPE_WKS        uint16 = 11
+	TYPE_PTR        uint16 = 12
+	TYPE_HINFO      uint16 = 13
+	TYPE_MINFO      uint16 = 14
+	TYPE_MX         uint16 = 15
+	TYPE_TXT        uint16 = 16
+	TYPE_RP         uint16 = 17
+	TYPE_AFSDB      uint16 = 18
+	TYPE_X25        uint16 = 19
+	TYPE_ISDN       uint16 = 20
+	TYPE_RT         uint16 = 21
+	TYPE_NSAP       uint16 = 22
+	TYPE_NSAPPTR    uint16 = 23
+	TYPE_SIG        uint16 = 24
+	TYPE_KEY        uint16 = 25
+	TYPE_PX         uint16 = 26
+	TYPE_GPOS       uint16 = 27
+	TYPE_AAAA       uint16 = 28
+	TYPE_LOC        uint16 = 29
+	TYPE_NXT        uint16 = 30
+	TYPE_EID        uint16 = 31
+	TYPE_NIMLOC     uint16 = 32
+	TYPE_SRV        uint16 = 33
+	TYPE_ATMA       uint16 = 34
+	TYPE_NAPTR      uint16 = 35
+	TYPE_KX         uint16 = 36
+	TYPE_CERT       uint16 = 37
+	TYPE_DNAME      uint16 = 39
+	TYPE_OPT        uint16 = 41 // EDNS
+	TYPE_DS         uint16 = 43
+	TYPE_SSHFP      uint16 = 44
+	TYPE_IPSECKEY   uint16 = 45
+	TYPE_RRSIG      uint16 = 46
+	TYPE_NSEC       uint16 = 47
+	TYPE_DNSKEY     uint16 = 48
+	TYPE_DHCID      uint16 = 49
+	TYPE_NSEC3      uint16 = 50
+	TYPE_NSEC3PARAM uint16 = 51
+	TYPE_TLSA       uint16 = 52
+	TYPE_HIP        uint16 = 55
+	TYPE_NINFO      uint16 = 56
+	TYPE_RKEY       uint16 = 57
+	TYPE_TALINK     uint16 = 58
+	TYPE_CDS        uint16 = 59
+	TYPE_SPF        uint16 = 99
+	TYPE_UINFO      uint16 = 100
+	TYPE_UID        uint16 = 101
+	TYPE_GID        uint16 = 102
+	TYPE_UNSPEC     uint16 = 103
+	TYPE_NID        uint16 = 104
+	TYPE_L32        uint16 = 105
+	TYPE_L64        uint16 = 106
+	TYPE_LP         uint16 = 107
+	TYPE_EUI48      uint16 = 108
+	TYPE_EUI64      uint16 = 109
+
+	TYPE_TKEY uint16 = 249
+	TYPE_TSIG uint16 = 250
+
+	// valid Question.Type only
+	TYPE_IXFR  uint16 = 251
+	TYPE_AXFR  uint16 = 252
+	TYPE_MAILB uint16 = 253
+	TYPE_MAILA uint16 = 254
+	TYPE_ANY   uint16 = 255
+
+	TYPE_URI      uint16 = 256
+	TYPE_CAA      uint16 = 257
+	TYPE_TA       uint16 = 32768
+	TYPE_DLV      uint16 = 32769
+	TYPE_Reserved uint16 = 65535
+
+	// valid Question.Class and RRHeader.Class
+	CLASS_INET   = 1
+	CLASS_CSNET  = 2
+	CLASS_CHAOS  = 3
+	CLASS_HESIOD = 4
+	CLASS_NONE   = 254
+	CLASS_ANY    = 255
+
+	// Message.Hdr.Rcode
+	RCODE_SUCCESS        = 0
+	RCODE_FORMATERROR    = 1
+	RCODE_SERVERFAILURE  = 2
+	RCODE_NAMEERROR      = 3
+	RCODE_NOTIMPLEMENTED = 4
+	RCODE_REFUSED        = 5
+	RCODE_YXDOMAIN       = 6
+	RCODE_YXRRSET        = 7
+	RCODE_NXRRSET        = 8
+	RCODE_NOTAUTH        = 9
+	RCODE_NOTZONE        = 10
+	RCODE_BADSIG         = 16 // TSIG
+	RCODE_BADVERS        = 16 // EDNS0
+	RCODE_BADKEY         = 17
+	RCODE_BADTIME        = 18
+	RCODE_BADMODE        = 19 // TKEY
+	RCODE_BADNAME        = 20
+	RCODE_BADALG         = 21
+	RCODE_BADTRUNC       = 22 // TSIG
+
+	// OPCODE_
+	OPCODE_QUERY  = 0
+	OPCODE_IQUERY = 1
+	OPCODE_STATUS = 2
+	// There is no 3
+	OPCODE_NOTIFY = 4
+	OPCODE_UPDATE = 5
+)
--- dns/message.go
+++ dns/message.go
@@ -1,13 +1,8 @@
 package dns
 
 import (
-        "errors"
-        "net"
-)
-
-const (
-        QR_REQUEST  uint16 = 0
-        QR_RESPONSE uint16 = 1
+	"errors"
+	"net"
 )
 
 //                              1  1  1  1  1  1
@@ -26,21 +21,25 @@ const (
 //|                    ARCOUNT                    |
 //+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
 const HEADER_LENGTH = 12
+const (
+	IPV4_LEN = 4
+	IPV6_LEN = 16
+)
 
 type Header struct {
-        Id                 uint16
-        QueryResponse      uint8 //request or response
-        Opcode             uint8
-        AuthAnswer         bool
-        Truncated          bool
-        RecursionDesired   bool
-        RecursionAvailable bool
-        Zero               uint8
-        Rcode              uint8
-        QDCount            uint16
-        ANCount            uint16
-        NSCount            uint16
-        ARCount            uint16
+	Id                 uint16
+	QueryResponse      uint8 //request or response
+	Opcode             uint8
+	AuthAnswer         bool
+	Truncated          bool
+	RecursionDesired   bool
+	RecursionAvailable bool
+	Zero               uint8
+	Rcode              uint8
+	QDCount            uint16
+	ANCount            uint16
+	NSCount            uint16
+	ARCount            uint16
 }
 
 //                              1  1  1  1  1  1
@@ -55,9 +54,9 @@ type Header struct {
 //|                     QCLASS                    |
 //+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
 type Question struct {
-        Name   string `dns:"cdomain-name"` // "cdomain-name" specifies encoding (and may be compressed)
-        QType  uint16
-        QClass uint16
+	Name  string `dns:"cdomain-name"` // "cdomain-name" specifies encoding (and may be compressed)
+	Type  uint16
+	Class uint16
 }
 
 //                              1  1  1  1  1  1
@@ -81,20 +80,20 @@ type Question struct {
 ///                                               /
 //+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
 
-type RRBase struct {
-        Name     string `dns:"cdomain-name"`
-        Type     uint16
-        Class    uint16
-        Ttl      uint32
-        RdLength uint16 // length of data after header
+type RRHeader struct {
+	Name     string `dns:"cdomain-name"`
+	Type     uint16
+	Class    uint16
+	Ttl      uint32
+	RdLength uint16 // length of data after header
 }
 
 type RR interface {
 }
 
 type A struct {
-        RRBase
-        IPv4 net.IP
+	Hdr  RRHeader
+	IPv4 net.IP
 }
 
 //+---------------------+
@@ -109,171 +108,205 @@ type A struct {
 //|      Additional     | RRs holding additional information
 //+---------------------+
 type Message struct {
-        Hdr        Header
-        Question   []Question // Holds the RR(s) of the question section.
-        Answer     []RR       // Holds the RR(s) of the answer section.
-        Authority  []RR       // Holds the RR(s) of the authority section.
-        Additional []RR       // Holds the RR(s) of the additional section.
+	Hdr        Header
+	Question   []Question // Holds the RR(s) of the question section.
+	Answer     []RR       // Holds the RR(s) of the answer section.
+	Authority  []RR       // Holds the RR(s) of the authority section.
+	Additional []RR       // Holds the RR(s) of the additional section.
 }
 
 // Unpack a binary message to a Msg structure.
 func (msg *Message) UnpackHeaderAndQuestion(data []byte) (offset int, err error) {
-        if len(data) < HEADER_LENGTH {
-                err = errors.New("message data too short")
-                return
-        }
-        const (
-                _QUREY_RESPONSE      = 0x80
-                _OPCODE              = 0x78
-                _AUTH_ANSWER         = 0x04
-                _TRUNCATED           = 0x02
-                _RECURSION_DESIRED   = 0x01
-                _RECURSION_AVAILABLE = 0x80
-                _ZERO                = 0x70
-                _RCODE               = 0x08
-        )
+	if len(data) < HEADER_LENGTH {
+		err = errors.New("message data too short")
+		return
+	}
+	const (
+		_QUREY_RESPONSE      = 0x80
+		_OPCODE              = 0x78
+		_AUTH_ANSWER         = 0x04
+		_TRUNCATED           = 0x02
+		_RECURSION_DESIRED   = 0x01
+		_RECURSION_AVAILABLE = 0x80
+		_ZERO                = 0x70
+		_RCODE               = 0x08
+	)
 
-        //unpack message hearder
-        offset = 0
-        msg.Hdr.Id, offset = unpackUint16(data, offset)
-        msg.Hdr.QueryResponse = uint8(data[offset]) & _QUREY_RESPONSE
-        msg.Hdr.Opcode = uint8(data[offset]) & _OPCODE
-        msg.Hdr.AuthAnswer = uint8(data[offset])&_AUTH_ANSWER == 1
-        msg.Hdr.Truncated = uint8(data[offset])&_TRUNCATED == 1
-        msg.Hdr.RecursionDesired = uint8(data[offset])&_RECURSION_DESIRED == 1
-        offset += 1
-        msg.Hdr.RecursionAvailable = uint8(data[offset])&_RECURSION_AVAILABLE == 1
-        msg.Hdr.Zero = uint8(data[offset]) & _ZERO
-        msg.Hdr.Rcode = uint8(data[offset]) & _RCODE
-        offset += 1
-        msg.Hdr.QDCount, offset = unpackUint16(data, offset)
-        msg.Hdr.ANCount, offset = unpackUint16(data, offset)
-        msg.Hdr.NSCount, offset = unpackUint16(data, offset)
-        msg.Hdr.ARCount, offset = unpackUint16(data, offset)
+	//unpack message hearder
+	offset = 0
+	msg.Hdr.Id, offset = unpackUint16(data, offset)
+	msg.Hdr.QueryResponse = uint8(data[offset]) & _QUREY_RESPONSE
+	msg.Hdr.Opcode = uint8(data[offset]) & _OPCODE
+	msg.Hdr.AuthAnswer = uint8(data[offset])&_AUTH_ANSWER == 1
+	msg.Hdr.Truncated = uint8(data[offset])&_TRUNCATED == 1
+	msg.Hdr.RecursionDesired = uint8(data[offset])&_RECURSION_DESIRED == 1
+	offset += 1
+	msg.Hdr.RecursionAvailable = uint8(data[offset])&_RECURSION_AVAILABLE == 1
+	msg.Hdr.Zero = uint8(data[offset]) & _ZERO
+	msg.Hdr.Rcode = uint8(data[offset]) & _RCODE
+	offset += 1
+	msg.Hdr.QDCount, offset = unpackUint16(data, offset)
+	msg.Hdr.ANCount, offset = unpackUint16(data, offset)
+	msg.Hdr.NSCount, offset = unpackUint16(data, offset)
+	msg.Hdr.ARCount, offset = unpackUint16(data, offset)
 
-        // Arrays.
-        msg.Question = make([]Question, msg.Hdr.QDCount)
+	// Arrays.
+	msg.Question = make([]Question, msg.Hdr.QDCount)
 
-        for i := uint16(0); i < msg.Hdr.QDCount; i++ {
-                offset, err = unpackQuestion(data, offset, &msg.Question[i])
-                if err != nil {
-                        return
-                }
-        }
-        return
+	for i := uint16(0); i < msg.Hdr.QDCount; i++ {
+		offset, err = unpackQuestion(data, offset, &msg.Question[i])
+		if err != nil {
+			return
+		}
+	}
+	return
 }
 
 func (msg *Message) UnpackAll(data []byte) (err error) {
-        var offset int
-        offset, err = msg.UnpackHeaderAndQuestion(data)
-        if err != nil {
-                return
-        }
-        msg.Answer = make([]RR, msg.Hdr.ANCount)
-        msg.Authority = make([]RR, msg.Hdr.NSCount)
-        msg.Additional = make([]RR, msg.Hdr.ARCount)
+	var offset int
+	offset, err = msg.UnpackHeaderAndQuestion(data)
+	if err != nil {
+		return
+	}
+	msg.Answer = make([]RR, msg.Hdr.ANCount)
+	msg.Authority = make([]RR, msg.Hdr.NSCount)
+	msg.Additional = make([]RR, msg.Hdr.ARCount)
 
-        for i := uint16(0); i < msg.Hdr.ANCount; i++ {
-                msg.Answer[i], offset, err = unpackRR(data, offset)
-                if err != nil {
-                        return
-                }
-        }
-        for i := uint16(0); i < msg.Hdr.NSCount; i++ {
-                msg.Authority[i], offset, err = unpackRR(data, offset)
-                if err != nil {
-                        return
-                }
-        }
-        for i := uint16(0); i < msg.Hdr.ARCount; i++ {
-                msg.Additional[i], offset, err = unpackRR(data, offset)
-                if err != nil {
-                        return
-                }
-        }
-        if offset != len(data) {
-                err = errors.New("message data too long")
-                return
-        }
-        return nil
+	for i := uint16(0); i < msg.Hdr.ANCount; i++ {
+		msg.Answer[i], offset, err = unpackRR(data, offset)
+		if err != nil {
+			return
+		}
+	}
+	for i := uint16(0); i < msg.Hdr.NSCount; i++ {
+		msg.Authority[i], offset, err = unpackRR(data, offset)
+		if err != nil {
+			return
+		}
+	}
+	for i := uint16(0); i < msg.Hdr.ARCount; i++ {
+		msg.Additional[i], offset, err = unpackRR(data, offset)
+		if err != nil {
+			return
+		}
+	}
+	if offset != len(data) {
+		err = errors.New("message data too long")
+		return
+	}
+	return nil
 }
 
 func unpackUint32(data []byte, index int) (value uint32, offset int) {
-        value = uint32(data[index])<<24 | uint32(data[index+1])<<16 | uint32(data[index+2])<<8 | uint32(data[index+3])
-        offset = index + 4
-        return
+	value = uint32(data[index])<<24 | uint32(data[index+1])<<16 | uint32(data[index+2])<<8 | uint32(data[index+3])
+	offset = index + 4
+	return
 }
 func unpackUint16(data []byte, index int) (value uint16, offset int) {
-        value = uint16(data[index])<<8 | uint16(data[index+1])
-        offset = index + 2
-        return
+	value = uint16(data[index])<<8 | uint16(data[index+1])
+	offset = index + 2
+	return
 }
 
 func unpackDomainName(data []byte, index int) (name string, offset int, err error) {
-        dataLen := len(data)
-        offset = index
-        for {
-                if offset+1 > dataLen {
-                        err = errors.New("out of range")
-                        return
-                }
-                labelLen := int(data[offset])
-                offset++
-                switch labelLen & 0xC0 {
-                case 0x00:
-                        // end of name
-                        if labelLen == 0x00 {
-                                if len(name) == 0 {
-                                        name = "."
-                                        return
-                                } else {
-                                        return
-                                }
-                        }
-                        if offset+labelLen > dataLen {
-                                err = errors.New("out of range")
-                                return
-                        }
-                        name += string(data[offset : offset+labelLen])
-                        name += "."
-                        offset += labelLen
-                case 0xC0:
-                        // pointer to somewhere else in msg.
-                        // FIXME maybe there's an infinite loop.
-                        if labelLen > dataLen {
-                                err = errors.New("ptr out of range")
-                                return
-                        }
-                        tempName, _, tempErr := unpackDomainName(data, labelLen)
-                        if tempErr != nil {
-                                return
-                        }
-                        name += tempName
-                        return
-                default:
-                        // 0x80 and 0x40 are reserved
-                        err = errors.New("fomart error")
-                        return
-                }
-        }
-        return
+	dataLen := len(data)
+	offset = index
+	for {
+		if offset+1 > dataLen {
+			err = errors.New("out of range")
+			return
+		}
+		labelLen := int(data[offset])
+		offset++
+		switch labelLen & 0xC0 {
+		case 0x00:
+			// end of name
+			if labelLen == 0x00 {
+				if len(name) == 0 {
+					name = "."
+					return
+				} else {
+					return
+				}
+			}
+			if offset+labelLen > dataLen {
+				err = errors.New("out of range")
+				return
+			}
+			name += string(data[offset : offset+labelLen])
+			name += "."
+			offset += labelLen
+		case 0xC0:
+			// pointer to somewhere else in msg.
+			// FIXME maybe there's an infinite loop.
+			if labelLen > dataLen {
+				err = errors.New("ptr out of range")
+				return
+			}
+			tempName, _, tempErr := unpackDomainName(data, labelLen)
+			if tempErr != nil {
+				return
+			}
+			name += tempName
+			return
+		default:
+			// 0x80 and 0x40 are reserved
+			err = errors.New("fomart error")
+			return
+		}
+	}
+	return
 }
 
 func unpackQuestion(data []byte, index int, question *Question) (offset int, err error) {
-        offset = index
-        question.Name, offset, err = unpackDomainName(data, offset)
-        if err != nil {
-                return
-        }
-        if offset+4 > len(data) {
-                err = errors.New("out of range")
-                return
-        }
-        question.QType, offset = unpackUint16(data, offset)
-        question.QClass, offset = unpackUint16(data, offset)
-        return
+	offset = index
+	question.Name, offset, err = unpackDomainName(data, offset)
+	if err != nil {
+		return
+	}
+	if offset+4 > len(data) {
+		err = errors.New("out of range")
+		return
+	}
+	question.Type, offset = unpackUint16(data, offset)
+	question.Class, offset = unpackUint16(data, offset)
+	return
+}
+
+type RRBase struct {
+	Name     string `dns:"cdomain-name"`
+	Type     uint16
+	Class    uint16
+	Ttl      uint32
+	RdLength uint16 // length of data after header
 }
 
 func unpackRR(data []byte, index int) (rr RR, offset int, err error) {
-        return
+	offset = index
+	var hdr RRHeader
+	hdr.Name, offset, err = unpackDomainName(data, offset)
+	if err != nil {
+		return
+	}
+	if offset+10 > len(data) {
+		err = errors.New("out of range")
+		return
+	}
+	hdr.Type, offset = unpackUint16(data, offset)
+	hdr.Class, offset = unpackUint16(data, offset)
+	hdr.Ttl, offset = unpackUint32(data, offset)
+	hdr.RdLength, offset = unpackUint16(data, offset)
+	if hdr.Class != CLASS_INET && hdr.Type != TYPE_A {
+		err = errors.New("unimplement")
+		return
+	}
+	if hdr.RdLength != IPV4_LEN {
+		err = errors.New("formart error")
+		return
+	}
+	var a A
+	a.Hdr = hdr
+	a.IPv4 = net.IPv4(data[offset], data[offset+1], data[offset+2], data[offset+3])
+	offset += IPV4_LEN
+	return
 }
