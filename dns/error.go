3e1c5b8b1b0dac5a0703913912d5ea2e1f57e2bb
young.wu <public0821@gmail.com>
2014-01-15 18:04:56 +0800

    Update error.go

--- dns/error.go
+++ dns/error.go
@@ -1,5 +1,32 @@
-package dns
+package main
+
+import (
+    "fmt"
+    "runtime"
+)
+
+func wrapperErr(text, wrapper string) (output string) {
+    _, file, line, ok := runtime.Caller(2)
+    if !ok {
+        file = "???"
+        line = 0
+    }
+    output = fmt.Sprintf("%s %s:%d %s", wrapper, file, line, text)
+    return
+}
+
+func NewDnsError(text string) (err error) {
+    return &DnsError{wrapperErr("dns", text)}
+}
 
 type DnsError struct {
-	message string
+    err string
+}
+
+func (e *DnsError) Error() string {
+    return e.err
+}
+
+type UnimplementedError struct {
+    DnsError
 }
